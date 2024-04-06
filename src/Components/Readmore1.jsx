59dea05f59fcbbc28d1bd4f3ef98655af4c4c178
Sumit Gautam <141570316+CodeTimus@users.noreply.github.com>
2024-04-06 11:10:07 +0530

    update

---
+++ src/Components/Readmore1.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Readmore1 = () => {
+  return (
+    <div>
+        <>
+  <meta charSet="UTF-8" />
+  <link rel="stylesheet" href="Readmore1.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  
+  <title>Document</title>
+  <body>
+    
+ 
+  <section className="section__container about__container">
+    <div className="about__content">
+      <h2 className="section__header">About Us</h2>
+      <p>
+        Welcome to our healthcare website, your one-stop destination for
+        reliable and comprehensive health care information. We are committed to
+        promoting wellness and providing valuable resources to empower you on
+        your health journey.
+      </p>
+      <p>
+        Explore our extensive collection of expertly written articles and guides
+        covering a wide range of health topics. From understanding common
+        medical conditions to tips for maintaining a healthy lifestyle, our
+        content is designed to educate, inspire, and support you in making
+        informed choices for your health.
+      </p>
+      <p>
+        Discover practical health tips and lifestyle advice to optimize your
+        physical and mental well-being. We believe that small changes can lead
+        to significant improvements in your quality of life, and we're here to
+        guide you on your path to a healthier and happier you.
+      </p>
+    </div>
+    <div className="about__image">
+    <img src="images/about.jpg" alt="" />
+    </div>
+  </section>
+    </body>
+</>
+
+    </div>
+  )
+}
+
+export default Readmore1
