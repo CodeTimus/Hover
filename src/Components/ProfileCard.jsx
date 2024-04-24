5c9e39df39bb990a09d20d9a7adf56f654280b16
Areeba Shakeel <shakeelareeba02@gmail.com>
2024-04-24 10:51:21 +0530

    profile page

--- src/Components/ProfileCard.jsx
+++ src/Components/ProfileCard.jsx
@@ -1,156 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ProfileCard.css";
 const ProfileCard = () => {
-  return (
-    <div>
-      <>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="style.css" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        />
-        <title>Document</title>
 
-        <div className="row">
-          <div className="col-md-3">
-            <div className="box88">
-              <div className="profile-box88">
-                <div className="profile-img88">
-                  <img src="images/user-3.jpg" alt="User Image" />
-                </div>
-                <div className="profile-docs88">
-                  <h1>Sandra D. Johnson</h1>
-                  <h3>Frontend Developer</h3>
-                  <p>
-                    A front-end developer is a software developer. Who works on
-                    the client-side, of a web application.
-                  </p>
-                  <button>View More</button>
-                  <ul className="profile-icon88">
-                    <li>
-                      <i className="fa-brands fa-github" />
-                    </li>
-                    <li>
-                      <i className="fa-brands fa-instagram" />
-                    </li>
-                    <li>
-                      <i className="fa-brands fa-linkedin" />
-                    </li>
-                    <li>
-                      <i className="fa-brands fa-twitter" />
-                    </li>
-                  </ul>
-                </div>
+  const [doctor, setDoctor] = useState([]);
+
+  //   const { category } = useParams();
+
+  const fetchDoctor = async () => {
+    const res = await fetch("http://localhost:3000/doctor/getall");
+
+    console.log(res.status);
+
+    const data = await res.json();
+    console.log(data);
+    if (res.status === 200) {
+      // const data = await res.json();
+      console.log(data);
+      setDoctor(data)
+    }
+  };
+
+  useEffect(() => {
+    fetchDoctor();
+  }, []);
+
+  const displayDoctor = () => {
+    return doctor.map((doc) => (
+      // <div className="conatainer">
+      //   <div className="col-md-4">
+      //     <div className="card p-3 mb-5 bg-dark p-card">
+      //       <h3 style={{ paddingLeft: "20px" }} className="mt-3 text-light">{obj.pname}</h3>
+      //       <p style={{ paddingLeft: "20px" }} className="text-light">{obj.pprice}</p>
+      //       <Link to={'/view/' + obj._id} className="btn btn-outline-primary m-2">Know More</Link>
+
+
+      //     </div>
+      //   </div>
+      // </div>
+
+        <div className="col-md-4">
+          <div className="box88">
+            <div className="profile-box88">
+              <div className="profile-img88">
+                <img src="images/user-3.jpg" alt="User Image" />
+              </div>
+              <div className="profile-docs88">
+                <h1>{doc.name}</h1>
+                <h3>{doc.speciality}</h3>
+                <p>
+                  A front-end developer is a software developer. Who works on
+                  the client-side, of a web application.
+                </p>
+                <button>View More</button>
               </div>
             </div>
-          </div>
-
-          <div className="col-md-3">
-            <div className="box88">
-              <div className="profile-box88">
-                <div className="profile-img88">
-                  <img src="images/user-4.jpg" alt="User Image" />
-                </div>
-                <div className="profile-docs88">
-                  <h1>Sandra D. Johnson</h1>
-                  <h3>Frontend Developer</h3>
-                  <p>
-                    A front-end developer is a software developer. Who works on
-                    the client-side, of a web application.
-                  </p>
-                  <button>View More</button>
-                  <ul className="profile-icon88">
-                    <li>
-                      <i className="fa-brands fa-github" />
-                    </li>
-                    <li>
-                      <i className="fa-brands fa-instagram" />
-                    </li>
-                    <li>
-                      <i className="fa-brands fa-linkedin" />
-                    </li>
-                    <li>
-                      <i className="fa-brands fa-twitter" />
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* <div className="col-md-3">
-          <div className="box88">
-    <div className="profile-box88">
-      <div className="profile-img88">
-        <img src="images/user-1.png" alt="User Image" />
-      </div>
-      <div className="profile-docs88">
-        <h1>Sandra D. Johnson</h1>
-        <h3>Frontend Developer</h3>
-        <p>
-          A front-end developer is a software developer. Who works on the
-          client-side, of a web application.
-        </p>
-        <button>View More</button>
-        <ul className="profile-icon88">
-        <li>
-          <i className="fa-brands fa-github" />
-        </li>
-        <li>
-          <i className="fa-brands fa-instagram" />
-        </li>
-        <li>
-          <i className="fa-brands fa-linkedin" />
-        </li>
-        <li>
-          <i className="fa-brands fa-twitter" />
-        </li>
-      </ul>
-      </div>
-
-    </div>
-  </div>
-          </div> */}
-          <div className="col-md-3">
-          <div className="box88">
-    <div className="profile-box88">
-      <div className="profile-img88">
-        <img src="images/user-1.png" alt="User Image" />
-      </div>
-      <div className="profile-docs88">
-        <h1>Sandra D. Johnson</h1>
-        <h3>Frontend Developer</h3>
-        <p>
-          A front-end developer is a software developer. Who works on the
-          client-side, of a web application.
-        </p>
-        <button>View More</button>
-        <ul className="profile-icon88">
-        <li>
-          <i className="fa-brands fa-github" />
-        </li>
-        <li>
-          <i className="fa-brands fa-instagram" />
-        </li>
-        <li>
-          <i className="fa-brands fa-linkedin" />
-        </li>
-        <li>
-          <i className="fa-brands fa-twitter" />
-        </li>
-      </ul>
-      </div>
-
-    </div>
-  </div>
           </div>
         </div>
 
 
-      </>
+
+    ));
+  }
+
+  return (
+    <div>
+       <header className='bg-body-tertiory'>
+                <div className='container py-5'>
+
+                    <p className='display-2 text-center mb-5 fw-bold'>All Products</p>
+
+                    <input type="text" placeholder='Search Items' className='form-control w-75 m-auto'  />
+
+                </div>
+            </header >
+      <div className='row mt-5  d-flex justify-content-center'> {displayDoctor()} </div>
     </div>
   );
 };
