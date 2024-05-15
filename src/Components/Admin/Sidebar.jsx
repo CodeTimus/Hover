1dbbfee380329c36363cd35866cd0877d2a52cd0
Areeba Shakeel <shakeelareeba02@gmail.com>
2024-05-15 09:42:25 +0530

    sidebar

--- src/Components/Admin/Sidebar.jsx
+++ src/Components/Admin/Sidebar.jsx
@@ -7,141 +7,85 @@ const Sidebar = ({ children }) => {
 
   return (
     <>
-   <>
-  <div className="sidebar">
-    <div className="logo-details">
-      <i className="bx bxl-c-plus-plus icon" />
-      <div className="logo_name">EDS</div>
-      <i className=" bx bx-menu" id="btn"  />
-    </div>
-    <ul className="nav-list">
-        <li>
-        <Link to="/admin/AdminDashboard">
-          <i className="bx bx-grid-alt" />
-          <span className="links_name">Admin Dashboard</span>
-        </Link>
-        <span className="tooltip">Admin Dashboard</span>
-      </li>
-      <li>
-        <Link to="/Admin/ManageUser">
-          <i className="bx bx-user" />
-          <span className="links_name">Manage User</span>
-        </Link>
-        <span className="tooltip">Manage User</span>
-      </li>
-      <li>
-        <Link to="/Admin/AddDoctor">
-          <i className="bx bxs-cart-add" />
-          <span className="links_name">Add Product</span>
-        </Link>
-        <span className="tooltip">Add Product</span>
-      </li>
-      <li>
-        <Link to="/Admin/ManageProduct">
-          <i className="bx bx-pie-chart-alt-2" /> 
-          <span className="links_name">Manage Product</span>
-        </Link>
-        <span className="tooltip">Manage Product</span>
-      </li>
 
-      <li>
-        <Link to="/Admin/ManageOrder">
-          <i className="bx bx-cart-alt" />
-          <span className="links_name">Manage Order</span>
-        </Link>
-        <span className="tooltip">Manage Order</span>
-      </li>
-   
-      <li>
-        <a href="#">
-          <i className="bx bx-cog" />
-          <span className="links_name">Setting</span>
-        </a>
-        <span className="tooltip">Setting</span>
-      </li>
-      <li className="profile">
-        <div className="profile-details">
-          <img
-            src="https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297_640.png"
-            alt="profileImg"
-          />
-          <div className="name_job">
-            <div className="name">Stella Army</div>
-            <div className="job">Web designer</div>
+      <>
+        <div className="sidebar">
+          <div className="logo-details">
+            <i className="bx bxl-c-plus-plus icon" />
+            <div className="logo_name">Mitti Mahal</div>
+            <i className=" bx bx-menu" id="btn" />
           </div>
-      <div className="sidebar">
-        <div className="logo-details">
-          <i className="bx bxl-c-plus-plus icon" />
-          <div className="logo_name">Mitti Mahal</div>
-          <i className=" bx bx-menu" id="btn" />
-        </div>
-        <ul className="nav-list">
-          <li>
-            <Link to="/admin/AdminDashboard">
-              <i className="bx bx-grid-alt" />
-              <span className="links_name">Admin Dashboard</span>
-            </Link>
-            <span className="tooltip">Admin Dashboard</span>
-          </li>
-          <li>
-            <Link to="/Admin/ManageUser">
-              <i className="bx bx-user" />
-              <span className="links_name">Manage User</span>
-            </Link>
-            <span className="tooltip">Manage User</span>
-          </li>
-          <li>
-            <Link to="/Admin/AddProduct">
-              <i className="bx bxs-cart-add" />
-              <span className="links_name">Add Product</span>
-            </Link>
-            <span className="tooltip">Add Product</span>
-          </li>
-          <li>
-            <Link to="/Admin/ManageProduct">
-              <i className="bx bx-pie-chart-alt-2" />
-              <span className="links_name">Manage Product</span>
-            </Link>
-            <span className="tooltip">Manage Product</span>
-          </li>
+          <ul className="nav-list">
+            <li>
+              <Link to="/Admin/AdminDashboard">
+                <i className="bx bx-grid-alt" />
+                <span className="links_name">Admin Dashboard</span>
+              </Link>
+              <span className="tooltip">Admin Dashboard</span>
+            </li>
+            <li>
+              <Link to="/Admin/ManageUser">
+                <i className="bx bx-user" />
+                <span className="links_name">Manage User</span>
+              </Link>
+              <span className="tooltip">Manage User</span>
+            </li>
+            <li>
+              <Link to="/Admin/AddProduct">
+                <i className="bx bxs-cart-add" />
+                <span className="links_name">Add Product</span>
+              </Link>
+              <span className="tooltip">Add Product</span>
+            </li>
+            <li>
+              <Link to="/Admin/ManageProduct">
+                <i className="bx bx-pie-chart-alt-2" />
+                <span className="links_name">Manage Product</span>
+              </Link>
+              <span className="tooltip">Manage Product</span>
+            </li>
 
-          <li>
-            <Link to="/Admin/ManageOrder">
-              <i className="bx bx-cart-alt" />
-              <span className="links_name">Manage Order</span>
-            </Link>
-            <span className="tooltip">Manage Order</span>
-          </li>
+            <li>
+              <Link to="/Admin/ManageOrder">
+                <i className="bx bx-cart-alt" />
+                <span className="links_name">Manage Order</span>
+              </Link>
+              <span className="tooltip">Manage Order</span>
+            </li>
 
-          <li>
-            <a href="#">
-              <i className="bx bx-cog" />
-              <span className="links_name">Setting</span>
-            </a>
-            <span className="tooltip">Setting</span>
-          </li>
-          <li className="profile">
-            <div className="profile-details">
-              <img
-                src="https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297_640.png"
-                alt="profileImg"
-              />
-              <div className="name_job">
-                <div className="name">Stella Army</div>
-                <div className="job">Web designer</div>
+            <li>
+              <a href="#">
+                <i className="bx bx-cog" />
+                <span className="links_name">Setting</span>
+              </a>
+              <span className="tooltip">Setting</span>
+            </li>
+            <li className="profile">
+              <div className="profile-details">
+                <img
+                  src="https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297_640.png"
+                  alt="profileImg"
+                />
+                <div className="name_job">
+                  <div className="name">Stella Army</div>
+                  <div className="job">Web designer</div>
+                </div>
               </div>
-            </div>
-            <i className="bx bx-log-out" id="log_out" />
-          </li>
-        </ul>
-      </div>
-      {children}
-      {/* <section className="home-section">
+              <i className="bx bx-log-out" id="log_out" />
+            </li>
+          </ul>
+        </div>
+        {/* <section className="home-section">
+     <div className="text">Hello</div>
+   </section> */}
+      </>
+
+    </>
+  )
+  { children }
+}
+{/* <section className="home-section">
     <div className="text">Hello</div>
   </section> */}
-    </>
-
-  )
-}
 
 export default Sidebar
