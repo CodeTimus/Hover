aec347bde58cbf57a6d3b9c61eb7d68362f04fc1
CodeNova-Kumar <KUMARVERMA2002@GMAIL.COM>
2024-05-23 17:36:39 +0530

    ok

--- src/Components/main/Home.jsx
+++ src/Components/main/Home.jsx
@@ -247,48 +247,6 @@ const Home = () => {
         </section>
 
         {/* new One */}
-
-        {/* <section class="section__container service__container">
-      <div class="service__header">
-        <div class="service__header__content">
-          <h2 class="section__header">Our Special service</h2>
-          <p>
-            Beyond simply providing medical care, our commitment lies in
-            delivering unparalleled service tailored to your unique needs.
-          </p>
-        </div>
-        <button class="service__btn">Ask A Service</button>
-      </div>
-      <div class="service__grid">
-        <div class="service__card">
-          <span><i class="ri-microscope-line"></i></span>
-          <h4>Laboratory Test</h4>
-          <p>
-            Accurate Diagnostics, Swift Results: Experience top-notch Laboratory
-            Testing at our facility.
-          </p>
-          <a href="#">Learn More</a>
-        </div>
-        <div class="service__card">
-          <span><i class="ri-mental-health-line"></i></span>
-          <h4>Health Check</h4>
-          <p>
-            Our thorough assessments and expert evaluations help you stay
-            proactive about your health.
-          </p>
-          <a href="#">Learn More</a>
-        </div>
-        <div class="service__card">
-          <span><i class="ri-hospital-line"></i></span>
-          <h4>General Dentistry</h4>
-          <p>
-            Experience comprehensive oral care with Dentistry. Trust us to keep
-            your smile healthy and bright.
-          </p>
-          <a href="#">Learn More</a>
-        </div>
-      </div>
-    </section> */}
         {/* our doctors ended */}
         {/* our services */}
         <div className="our-service">
